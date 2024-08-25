b94a8214065b355d6d8e58e59783aa7554eb81e4
Ganny <62664788+Ganny-Abdeslam@users.noreply.github.com>
2024-08-25 11:08:51 -0500

    Create 10071-BacktoHighSchoolPhysics.cpp

---
+++ Introduccion/10071-BacktoHighSchoolPhysics.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, t;
+
+    while(scanf("%d %d",&n,&t) == 2){
+        printf("%d\n",n*t*2);
+    }
+    return 0;
+}#include <stdio.h>
+
+int main() {
+    int n, t;
+
+    while(scanf("%d %d",&n,&t) == 2){
+        printf("%d\n",n*t*2);
+    }
+    return 0;
+}
