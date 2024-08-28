d29033343d6936db6c55211972770e2824fb7088
Ganny <gannyser@gmail.com>
2024-08-27 19:22:46 -0500

    arreglos

--- Introduccion/10071-BacktoHighSchoolPhysics.cpp
+++ Introduccion/10071-BacktoHighSchoolPhysics.cpp
@@ -7,13 +7,4 @@ int main() {
         printf("%d\n",n*t*2);
     }
     return 0;
-}#include <stdio.h>
-
-int main() {
-    int n, t;
-
-    while(scanf("%d %d",&n,&t) == 2){
-        printf("%d\n",n*t*2);
-    }
-    return 0;
 }
